8a87b3e783a25acf9a757081547064ccccf20352
sodiqardianto <sodiqardianto@gmail.com>
2023-08-04 01:11:52 +0700

    Update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +15,11 @@ class Post extends Model
         'title',
         'content'
     ];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            fn ($image) => asset('/storage/posts/' . $image)
+        );
+    }
 }
